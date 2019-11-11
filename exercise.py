a3b2e51f02f8af5f90707d953dbc2b97672aa20f
SimplyAvi <a.totaram@gmail.com>
2019-11-11 07:00:58 -0500

    will this finially work?

---
+++ exercise.py
@@ -0,0 +1,6 @@
+# coding exercise : print formatting
+'you can put anything in before and after in which ever order and get these expected result as in the word imported inbetween the curleys =] {}'.format('Python rules!')
+# added a code for .format and use cases for possible email responses
+
+
+# added some packages to help format python on vsc?
--- helloWorld.py
+++ helloWorld.py
@@ -2,3 +2,7 @@ print('hello world from pythons code in terminal')
 # ran python within terminal - python helloWorld.py
 
 print('can i see this on github?')
+
+
+
+

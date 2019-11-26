de033c01573663ab9ad0aff0bd0a1f0e1fae954d
SimplyAvi <a.totaram@gmail.com>
2019-11-25 20:53:44 -0500

    lesser of two evens problem and solution

--- helloWorld.py
+++ helloWorld.py
@@ -40,6 +40,23 @@ print('have to work on git merge to check the differences between one branch on 
 # doing simple math with functions
 
 
-def myfunc(arg1, arg2):
-    return arg1 + arg2
+# def myfunc(arg1, arg2):
+#     return arg1 + arg2
 
+# LESSER OF TWO EVENS: Write a function that returns the lesser of two given numbers if both numbers are even, but returns the greater if one or both numbers are odd
+def lesser_of_two_evens(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        #         both numbers are even
+        if a < b:
+            result = a
+        else:
+            result = b
+#             you can return min(a,b)
+    else:
+        #     numbers are odd
+        if a < b:
+            result = b
+        else:
+            result = a
+#             you can return max(a,b)
+    return result

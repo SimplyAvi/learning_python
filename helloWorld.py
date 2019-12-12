50c920b15eec4ffb6fe1ca48f90a382250080c56
SimplyAvi <a.totaram@gmail.com>
2019-12-12 07:21:08 -0500

    created calulations for the area and volume of a cylinder

--- helloWorld.py
+++ helloWorld.py
@@ -202,14 +202,30 @@ print('have to work on git merge to check the differences between one branch on 
 #     return len(primes)
 
 # Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
-class Line:
+# class Line:
 
-    def __init__(self, coor1, coor2):
-        self.coor1 = coor1
-        self.coor2 = coor2
+#     def __init__(self, coor1, coor2):
+#         self.coor1 = coor1
+#         self.coor2 = coor2
 
-    def distance(self):
-        return ((self.coor1[1] - self.coor2[1]) ** 2 + (self.coor1[0] - self.coor2[0])**2)**.5
+#     def distance(self):
+#         return ((self.coor1[1] - self.coor2[1]) ** 2 + (self.coor1[0] - self.coor2[0])**2)**.5
 
-    def slope(self):
-        return ((self.coor2[1] - self.coor1[1])/((self.coor2[0] - self.coor1[0])))
+#     def slope(self):
+#         return ((self.coor2[1] - self.coor1[1])/((self.coor2[0] - self.coor1[0])))
+# 
+
+
+
+# calculations for a cylinder 
+class Cylinder:
+
+    def __init__(self, height=1, radius=1):
+        self.height = height
+        self.radius = radius
+
+    def volume(self):
+        return 3.14 * self.radius**2 * self.height
+
+    def surface_area(self):
+        return 2 * 3.14 * self.radius * self.height + 2 * 3.14 * self.radius**2

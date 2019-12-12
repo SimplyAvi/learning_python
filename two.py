e38ccf28c790c8037b76d7ed91229adfc45b4810
SimplyAvi <a.totaram@gmail.com>
2019-12-12 09:28:00 -0500

    description of another way you can exicute code"

--- two.py
+++ two.py
@@ -12,3 +12,7 @@ if __name__ == "__main__":
     print('two.py is run directly ')
 else:
     print('two.py has been imported')
+    
+# another way to do this is to have the funtion defined above 
+# then running the code under is instead of an else statement
+# which allows you to exicute code in a certin order

b05481b6bc1f923f258882853219e9f9ef5941f5
SimplyAvi <a.totaram@gmail.com>
2019-12-23 17:59:13 -0500

    capatalize doesnt wor for first letters of every word in each senteance so we use title instead

--- .vscode/Test_cap.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import Cap
-
-class TestCap(unittest.TestCase):
-    
-    def test_one_word(self):
-        text = 'python'
-        result = Cap.cap_text(text)
-        self.assertEqual(result, 'Python')
--- Cap.py
+++ Cap.py
@@ -3,4 +3,5 @@ def cap_text(text):
     input a string and return capatalize that string
     
     '''
-    return text.capitalize()
+    # return text.capitalize()
+    return text.title()

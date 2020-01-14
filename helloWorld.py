4793151ec064fab8ed93fb67d8c8171ab000a341
SimplyAvi <a.totaram@gmail.com>
2020-01-13 21:38:52 -0500

    used iter to itterate through a string

--- helloWorld.py
+++ helloWorld.py
@@ -484,14 +484,23 @@ print('have to work on git merge to check the differences between one branch on 
 # Create a generator that yields "n" random numbers between a low and high number (that are inputs).
 
 
-random.randint(1, 10)
+# random.randint(1, 10)
 
 
-def rand_num(low, high, n):
+# def rand_num(low, high, n):
 
-    for i in range(n):
-        yield random.randint(low, high)
+#     for i in range(n):
+#         yield random.randint(low, high)
 
 
-for num in rand_num(1, 10, 12):
-    print(num)
+# for num in rand_num(1, 10, 12):
+#     print(num)
+
+
+# Use the iter() function to convert the string below into an iterator:
+s = 'hello'
+
+s_iter = iter(s)
+
+for i in s_iter:
+    print(i)
